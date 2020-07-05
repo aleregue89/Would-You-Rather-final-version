7b9229e7ea92848a6d7fc87d232314baca9c1f46
Alejandro <aleregue89@gmail.com>
2020-07-05 19:00:40 -0400

    middleware

--- would-you-rather/src/reducers/index.js
+++ would-you-rather/src/reducers/index.js
@@ -1,8 +1,8 @@
 // What we do here is combine the reducers into one main root reducer which will combine the following into a single state object.
 
-import {combineReducers} from 'react-redux'
+import {combineReducers} from 'redux'
 import {users} from './users'
 import {questions} from './questions'
-import {authedUSer} from './authedUser'
+import {authedUser} from './authedUser'
 
-export default combineReducers({users, questions, authedUSer})
+export default combineReducers({users, questions, authedUser})
